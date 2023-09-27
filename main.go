6f1dd5032c92a1ae687a2e0c5b948bf88ce63872
Robert Palmer <61207668+Palm717@users.noreply.github.com>
2023-09-26 20:37:03 -0400

    static html

--- main.go
+++ main.go
@@ -2,14 +2,20 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+var tpl *template.Template
+
 func main() {
+
+	tpl, _ = template.ParseFiles("index.html")
 	// handle http req's at the path /hello
 	// ListenandServe starts the server HTTP serveer and listens for any req's on it's path
 	http.HandleFunc("/hello", helloHandleFunc) // helloHandleFunc is registered the /hello path
 	http.HandleFunc("/about", aboutHandleFunc)
+	http.HandleFunc("/", indexHandler)
 	http.ListenAndServe(":5555", nil) // nill invokes DefaultServeMux
 	// ServeMux is an HTTP request multiplexer. It matches the URL of each incoming req
 	// against a list of registered patterns and calls the handler for the pattern that closely matches
@@ -25,4 +31,8 @@ func helloHandleFunc(w http.ResponseWriter, r *http.Request) {
 
 func aboutHandleFunc(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "This is Go powered website")
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, nil)
 }
